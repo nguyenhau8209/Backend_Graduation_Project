04978576c03878df1707267f6974d50234014ee7
KienPC-KEN <phungchikien196@gmail.com>
2023-12-04 13:56:49 +0700

    new payment service

--- repositories/payment.repo.js
+++ repositories/payment.repo.js
@@ -1,5 +1,5 @@
 const db = require("../models");
-
+const { Op, fn, col } = require("sequelize");
 const createPayment = async ({
   orderId,
   paymentType,
@@ -20,5 +20,17 @@ const createPayment = async ({
   });
 };
 
-const paymentRepo = { createPayment };
+const statistics = async (filter = {}) => {
+  return await db.Payment.findAlld({
+    attributes: [[fn("SUM", col("total")), "totalPrice"]],
+    where: {
+      ...filter,
+      createdAt: {
+        [Op.between]: [filter.startDate, filter.endDate],
+      },
+    },
+  });
+};
+
+const paymentRepo = { createPayment, statistics };
 module.exports = paymentRepo;
