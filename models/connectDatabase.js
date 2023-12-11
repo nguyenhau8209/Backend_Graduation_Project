355f20d5b533a820be48202bc8099b52a98a7f68
nguyenhau8209 <conghaunguyen909@gmail.com>
2023-12-11 12:00:38 +0700

    update use db TiDB

--- models/connectDatabase.js
+++ models/connectDatabase.js
@@ -1,10 +1,24 @@
 const { Sequelize } = require("sequelize");
 const dotenv = require("dotenv");
+const fs = require("fs");
 dotenv.config();
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
+const sequelize = new Sequelize(process.env.TIDB_DB_NAME, process.env.TIDB_USER, process.env.TIDB_PASSWORD, {
+  host: process.env.TIDB_HOST,
   dialect: 'mysql',
+  port: process.env.TIDB_PORT,
   // Các tùy chọn Sequelize khác...
+  dialectOptions: {
+    ssl:
+        process.env?.TIDB_ENABLE_SSL === 'true'     // (Optional) Enable SSL
+            ? {
+              minVersion: 'TLSv1.2',
+              rejectUnauthorized: true,
+              ca: process.env.TIDB_CA_PATH          // (Optional) Path to the custom CA certificate
+                  ? fs.readFileSync(process.env.TIDB_CA_PATH)
+                  : undefined,
+            }
+            : null,
+  },
 });
 
 const connectionDatatbase = async () => {
